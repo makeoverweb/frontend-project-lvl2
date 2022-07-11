fe6a0d728e6b547cb63dcbea1f7ca1337aa52663
ssuslov <SSuslov@freeconferencecall.com>
2022-07-11 11:03:03 +0300

    init and first step

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import { Command } from "commander/esm.mjs";
+const program = new Command();
+
+program
+  .description("Compares two configuration files and shows a difference.")
+  .version("-V, --version output the version number");
+
+program.parse();
